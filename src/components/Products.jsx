17bd0ff02ec10dab94f87dcc9c56590973b391b7
Rohan Singh <rohannsingh98765@gmail.com>
2021-09-12 10:57:57 +0530

    adding products and styling

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,25 @@
+import PizzaProducts from "./PizzaProducts"
 
 const Products = () => {
     return (
-        <div>
-            <h1>This is prooducts List</h1>            
+        <div className="container mx-auto pb-24 px-4 md:px-0">
+            <h1 className="text-lg font-bold my-8">Our Delecious Menu </h1>            
+            <div className="grid grid-cols-2 my-g gap-24 md:grid-cols-5">
+                
+                <PizzaProducts/>
+                <PizzaProducts/>
+                <PizzaProducts/>
+                <PizzaProducts/>
+                <PizzaProducts/>
+                <PizzaProducts/>
+                <PizzaProducts/>
+                <PizzaProducts/>
+                <PizzaProducts/>
+                <PizzaProducts/>
+                <PizzaProducts/>
+
+                
+            </div>
         </div>
     )
 }
